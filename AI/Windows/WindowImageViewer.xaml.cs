ea3a794c7487c0d8c065badf7d1e1bca241c9497
Tommas <gersontommas@gmail.com>
2019-08-30 20:16:47 -0300

    No changes, Update anyway

--- AI/Windows/WindowImageViewer.xaml.cs
+++ AI/Windows/WindowImageViewer.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 
 namespace AI
 {
